78a61cb93a6a283a2cf7822f1cf40d2f10e63437
Daniel Malmgren de Oliveira <danmalmx@gmail.com>
2024-05-16 09:53:17 +0200

    Add characters

--- src/App.tsx
+++ src/App.tsx
@@ -1,23 +1,39 @@
 import { useEffect, useState } from "react";
 import "./App.css";
 
+type Character = {
+  name: string;
+  height: string;
+  mass: string;
+  hair_color: string;
+  skin_color: string;
+}
+
 function App() {
   const [films, setFilms] = useState([]);
+  const [characters, setChatacters] = useState<Character[]>([]);
+  const starWarsAPI = 'https://swapi.dev/api/'
+
+  const getFilmTitleSlug = (title: string) => {
+    return title.toLowerCase().replace(/ /g, "-");
+  };
+
 
   const getCharacters = async (film: { characters: string[] }) => {
-    const characters = await Promise.all(
+    const characters: Character[] = await Promise.all(
       film.characters.map(async (character: string) => {
         const response = await fetch(character);
         const data = await response.json();
         return data;
       })
     );
+    setChatacters(characters);
     console.log(characters);
   };
-
+ 
   useEffect(() => {
     const fetchTheDarkSide = async () => {
-      const response = await fetch("https://swapi.dev/api/films/");
+      const response = await fetch(`${starWarsAPI}/films`);
       const data = await response.json();
       setFilms(data.results);
       console.log(data.results);
@@ -26,11 +42,21 @@ function App() {
   }, []);
 
   return (
-    <>
-      {films.map((film: { title: string; characters: string[] }) => {
-        return <h1 onClick={() => getCharacters(film)}>{film.title}</h1>;
-      })}
-    </>
+    <div className="grid grid-cols-3 gap-x-4 gap-y-12 w-full justify-between px-12 ">
+      {films.map((film: { title: string; characters: string[] }) => (
+        <>
+        <div className="flex flex-col cursor-pointer" onClick={() => getCharacters(film)}>
+          <img src={`src/assets/${getFilmTitleSlug(film.title)}.jpg`} alt={`${film.title} image`} className="h-[500px] object-cover"/>
+          <h1 className="text-xl">{film.title}</h1>
+        </div>
+        </>
+      ))}
+      <div className="col-span-3 overflow-x-scroll flex">
+        {characters.map((character) => (
+            <div className="size-36 rounded-full bg-gray-500 mx-2">{character.name}</div>
+        ))}
+      </div>
+    </div>
   );
 }
 
